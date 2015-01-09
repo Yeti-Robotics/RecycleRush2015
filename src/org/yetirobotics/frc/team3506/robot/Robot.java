44eab6e04bbeabad5827806b62f7bec1a2d0358f
Trevor Daino <dainotrevor@gmail.com>
2015-01-08 20:09:48 -0500

    Prototype mecanum

--- src/org/yetirobotics/frc/team3506/robot/Robot.java
+++ src/org/yetirobotics/frc/team3506/robot/Robot.java
@@ -3,6 +3,7 @@ package org.yetirobotics.frc.team3506.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -16,29 +17,52 @@ public class Robot extends IterativeRobot {
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
+	// Declaration of various peripherals
 	Joystick leftJoy;
 	Joystick rightJoy;
 	RobotDrive drive;
+	Gyro gyro;
+	Talon leftBack;
+	Talon leftFront;
+	Talon rightBack;
+	Talon rightFront;
+	Timer timer;
+	Relay spike;
+	
+	// Integer ports according to roboRio setup
 	final int LEFT_JOYSTICK_PORT = 1;
 	final int RIGHT_JOYSTICK_PORT = 2;
 	final int LEFT_BACK_PORT = 4;
 	final int LEFT_FRONT_PORT = 3;
 	final int RIGHT_BACK_PORT = 2;
 	final int RIGHT_FRONT_PORT = 1;
-    private void mecanum (RobotDrive drive){
-    	
-    }
+	final int GYRO_PORT = 0; 
+	final int SPIKE_PORT = 0;
 	
-	/*
-	 * Runtime methods after this point
-	 * @see edu.wpi.first.wpilibj.IterativeRobot#robotInit()
-	 */
+	// Following speeds require double values between -1.0 and 1.0
+	final double X_SPEED = 0;
+	final double Y_SPEED = 0;
+	final double ROTATION_SPEED = 0;
+	
+	// Joystick orientation 
+	double leftX;
+	double leftY;
+	double rightX;
+	double rightY;
+	
+	// Supporting methods
+	
+	
+    // Runtime methods after this point
 	
 	public void robotInit() {
-    	leftJoy = new Joystick(LEFT_JOYSTICK_PORT);
+    	// Instantiation of various peripherals
+		leftJoy = new Joystick(LEFT_JOYSTICK_PORT);
     	rightJoy = new Joystick(RIGHT_JOYSTICK_PORT);
     	drive = new RobotDrive(LEFT_FRONT_PORT, LEFT_BACK_PORT, RIGHT_FRONT_PORT, RIGHT_BACK_PORT);
-    	
+    	gyro = new Gyro(GYRO_PORT);
+    	timer = new Timer();
+    	spike = new Relay(SPIKE_PORT);
     }
 
     /**
@@ -53,7 +77,14 @@ public class Robot extends IterativeRobot {
      */
     public void teleopPeriodic() {
     	while(isEnabled() && isOperatorControl()){
+    		leftX = leftJoy.getX();
+    		leftY = leftJoy.getY();
+    		rightX = rightJoy.getX();
+    		rightY =rightJoy.getY();
     		
+    		drive.mecanumDrive_Cartesian(leftX, rightX, leftY, gyro.getAngle());
+    		
+    		timer.delay(0.01);
     	}
     }
     
