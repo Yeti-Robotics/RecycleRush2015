33f6fe023b3d44d4e08c023e764c8a6a99ffbbc1
Trevor Daino <dainotrevor@gmail.com>
2015-01-08 19:21:02 -0500

    Initial commit; Motor and joystick ports set up

--- src/org/yetirobotics/frc/team3506/robot/Robot.java
+++ src/org/yetirobotics/frc/team3506/robot/Robot.java
@@ -2,6 +2,7 @@
 package org.yetirobotics.frc.team3506.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.*;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -15,8 +16,29 @@ public class Robot extends IterativeRobot {
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
-    public void robotInit() {
-
+	Joystick leftJoy;
+	Joystick rightJoy;
+	RobotDrive drive;
+	final int LEFT_JOYSTICK_PORT = 1;
+	final int RIGHT_JOYSTICK_PORT = 2;
+	final int LEFT_BACK_PORT = 4;
+	final int LEFT_FRONT_PORT = 3;
+	final int RIGHT_BACK_PORT = 2;
+	final int RIGHT_FRONT_PORT = 1;
+    private void mecanum (RobotDrive drive){
+    	
+    }
+	
+	/*
+	 * Runtime methods after this point
+	 * @see edu.wpi.first.wpilibj.IterativeRobot#robotInit()
+	 */
+	
+	public void robotInit() {
+    	leftJoy = new Joystick(LEFT_JOYSTICK_PORT);
+    	rightJoy = new Joystick(RIGHT_JOYSTICK_PORT);
+    	drive = new RobotDrive(LEFT_FRONT_PORT, LEFT_BACK_PORT, RIGHT_FRONT_PORT, RIGHT_BACK_PORT);
+    	
     }
 
     /**
@@ -30,14 +52,16 @@ public class Robot extends IterativeRobot {
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
-        
+    	while(isEnabled() && isOperatorControl()){
+    		
+    	}
     }
     
     /**
      * This function is called periodically during test mode
      */
     public void testPeriodic() {
-    
+    	
     }
     
 }
