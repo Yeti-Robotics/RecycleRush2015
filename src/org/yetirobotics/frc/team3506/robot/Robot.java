f1118a4353e84f853004f41def6441a32bf2c4ba
wazateer1 <wazateer1@gmail.com>
2015-01-23 19:06:34 -0500

    - Cleaned up unnecessary distractions

--- src/org/yetirobotics/frc/team3506/robot/Robot.java
+++ src/org/yetirobotics/frc/team3506/robot/Robot.java
@@ -2,6 +2,7 @@
 package org.yetirobotics.frc.team3506.robot;
 
 import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.Relay.Value;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
@@ -12,53 +13,11 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * directory.
  */
 public class Robot extends IterativeRobot {
-	Joystick leftJoy;
-	Joystick rightJoy;
-	RobotDrive drive;
-	Gyro gyro;
-	Talon leftBack;
-	Talon leftFront;
-	Talon rightBack;
-	Talon rightFront;
+
 	Relay spike;
-	
-	// Integer ports according to roboRio setup
-	final static int LEFT_JOYSTICK_PORT = 1;
-	final static int RIGHT_JOYSTICK_PORT = 2;
-	
-	// Talon ports 
-	final static int LEFT_BACK_PORT = 3;
-	final static int LEFT_FRONT_PORT = 2;
-	final static int RIGHT_BACK_PORT = 1;
-	final static int RIGHT_FRONT_PORT = 0;
-	
-	final static int GYRO_PORT = 0; 
-	final static int SPIKE_PORT = 0; // Not currently used
-	
-	// Following speeds require double values between -1.0 and 1.0
-	final static double X_SPEED = 0.25;
-	final static double Y_SPEED = 0.25;
-	final static double ROTATION_SPEED = 0.25;
-	
-	// Joystick orientation 
-	double leftX;
-	double leftY;
-	double rightX;
-	double rightY;
-	
-	int gyroResetTime = 5000;
-	long currentTime;
-	
-	final static double DEADZONE = 0.1;
-	
-	// Assisting methods
-	private double deadZoneMod(double joyVal){
-		if(Math.abs(joyVal)>0.1){
-			return joyVal;
-		} else{
-			return 0.0;
-		}
-	}
+	Joystick leftJoy;
+	I2C i2c;
+	byte[] toSend = new byte[1];
 	
     // Runtime methods after this point
     /**
@@ -67,12 +26,9 @@ public class Robot extends IterativeRobot {
      */
 	public void robotInit() {
     	// Instantiation of various peripherals
-		leftJoy = new Joystick(LEFT_JOYSTICK_PORT);
-    	rightJoy = new Joystick(RIGHT_JOYSTICK_PORT);
-    	gyro = new Gyro(GYRO_PORT);
-    	spike = new Relay(SPIKE_PORT);
-    	drive = new RobotDrive(LEFT_FRONT_PORT, LEFT_BACK_PORT, RIGHT_FRONT_PORT, RIGHT_BACK_PORT);
-    	gyro.reset();
+		leftJoy = new Joystick(1);
+		spike = new Relay(3);
+		i2c = new I2C(I2C.Port.kOnboard, 168);
     }
 
     /**
@@ -86,30 +42,15 @@ public class Robot extends IterativeRobot {
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
-    	currentTime = System.currentTimeMillis();
     	while(isEnabled() && isOperatorControl()){
-    		
-    		leftX = deadZoneMod(leftJoy.getX());  		
-    		leftY = deadZoneMod(leftJoy.getY());
-    		rightX = deadZoneMod(rightJoy.getX());
-    		rightY = deadZoneMod(rightJoy.getY());
-    		
-    		SmartDashboard.putString("victoria", "hello");
-    		
-    		drive.mecanumDrive_Cartesian(leftY, rightX, leftX, gyro.getAngle());
-        	
-        	SmartDashboard.putData("Gyro",gyro);
-    		SmartDashboard.putString("Hello","There");
-        	
-    		if(leftJoy.getRawButton(7)){
-    			gyro.reset();
+    		spike.set(Value.kForward);
+    		if(leftJoy.getRawButton(10)) {
+    			toSend[0] = 76;
     		}
-    		
-    		// Reset gyro automatically
-    		if((System.currentTimeMillis() - currentTime) > gyroResetTime){
-    			gyro.reset();
-    			currentTime = System.currentTimeMillis();
+    		else {
+    			toSend[0] = 72;
     		}
+    		i2c.transaction(toSend, 1, null, 0);
     	}
     }
     
